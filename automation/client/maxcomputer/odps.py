7b33a3f9382030db6c345b6f0482f53fd0099bfc
ZenRay <jake2011ren@gmail.com>
2023-04-11 23:50:18 +0800

    feat($maxcomputeClient): Maxcompute Client which can execute sql sentence

--- automation/client/maxcomputer/odps.py
+++ automation/client/maxcomputer/odps.py
@@ -3,10 +3,24 @@
 
 Load data and transform data by maxcomputer
 """
-
+import logging
 from odps import ODPS, options
 
 
+logger = logging.getLogger("automation.maxcomputerClient")
 class MaxComputerClient:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, *args, **kwargs) -> None:
+        self._client = ODPS(**kwargs)
+
+        logger.info("Load Macomputer Client Success")
+
+    
+    def execute_sql(self, sent, hints=None, **kwargs):
+        """Execute SQL Sentence"""
+        if hints is None:
+            self._client.execute_sql(sent)
+        else:
+            self._clli.execute_sql(sent, hints=hints)
+
+        logger.info("SQL Sentence Executed")
+    
