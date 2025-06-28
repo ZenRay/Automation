a6975a05b721619420df74e0ce751f1d6222c1be
ZenRay <jake2011ren@gmail.com>
2025-06-28 17:37:15 +0800

    Add Lark Exception

---
+++ automation/client/exceptions.py
@@ -0,0 +1,11 @@
+#coding:utf8
+
+
+
+class LarkException(Exception):
+    def __init__(self, code=0, msg=None):
+        self.code = code
+        self.msg = msg
+
+    def __str__(self) -> str:
+        return "{}:{}".format(self.code, self.msg)
