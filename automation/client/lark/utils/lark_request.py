8f0f953d0a90669c8530be65f72bb944ac73f3d1
ZenRay <jake2011ren@gmail.com>
2025-09-07 15:25:31 +0800

    Fix

--- automation/client/lark/utils/lark_request.py
+++ automation/client/lark/utils/lark_request.py
@@ -11,13 +11,13 @@ logger = logging.getLogger("automation.lark.utils.lark_request")
 
 
 
-def request(method, url, headers, payload={}, params=None, refresh_client=None, data=None):
+def request(method, url, headers, payload={}, params=None, refresh_client=None, data=None, proxy={}):
     """Lark API Request"""
     # Refresh Access Token if needed
     if refresh_client and hasattr(refresh_client, "_refresh_access_token") and callable(getattr(refresh_client, "_refresh_access_token")):
         refresh_client._refresh_access_token()
     response = requests.request(
-        method, url, headers=headers, json=payload, params=params, data=data
+        method, url, headers=headers, json=payload, params=params, data=data, proxies=proxy
     )
 
     resp = {}
