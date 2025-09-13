fbe80e11664dc11d3f3ba2abe0a1bbedd3f755bf
ZenRay <jake2011ren@gmail.com>
2025-09-13 12:12:12 +0800

    fix($dispatcher $larkoperator): Fix Date Value
    Parse Date Value To Int

--- dispatcher/operators/__init__.py
+++ dispatcher/operators/__init__.py
@@ -242,6 +242,12 @@ class LarkOperator(BaseOperator):
         # Update target URL
         lark_sheets.extract_spreadsheet_info(target_url)
         
+        # Fix Date Value to Int
+        if '日期' in df.columns:
+            df["日期"] = pd.to_datetime(df["日期"], errors='coerce').apply(
+                lambda x: x - lark_sheets._START_DATE if pd.notna(x) else x
+            ).dt.days
+            
         sheet_id = lark_sheets.get_sheet_id(sheet_title)
         raw_data = df.loc[:, columns].drop_duplicates().copy()
         
